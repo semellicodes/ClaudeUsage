41f5019811cd6e80d990da379a3b13518e56cc99
Paula Semelli Araújo do Amaral <paulaufra2023@gmail.com>
2026-09-10 23:53:59 -0300

    Cria projeto Xcode com target do app macOS

---
+++ ClaudeUsage/ClaudeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageApp.swift
+//  ClaudeUsage
+//
+//  Created by Paula Semelli Araújo do Amaral on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
